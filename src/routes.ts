6c218f5a9e18d3d124e6b9c59111a30ea4870d55
Fagner Medeiros <fagnermeds@gmail.com>
2020-08-24 15:53:33 -0300

    Initial commit

---
+++ src/routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.get('/users', (request, response) => {
+  return response.status(201).json([{
+    name: "Fagner",
+    email: "fagnermeds@gmail.com",
+    password: "123456",
+  },
+  {
+    name: "Maria",
+    email: "maria@gmail.com",
+    password: "maria123",
+  }
+]);
+});
+
+export default routes;
